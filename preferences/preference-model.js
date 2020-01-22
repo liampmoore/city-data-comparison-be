19a9d0230e4aa1c325721b0e4ceb8fa673bd12c3
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-01-22 13:43:53 -0800

    preference table

--- preferences/preference-model.js
+++ preferences/preference-model.js
@@ -1,2 +1,27 @@
 const db = require('../database/dbConfig.js')
 
+module.exports = {
+     insert,
+     findAll,
+     findByUser,
+     findById,
+     remove,
+     update
+}
+
+// TODO - POST new user preference
+
+
+// TODO - GET all user preferences
+
+
+// TODO - GET user preference under user's ID
+
+
+// TODO - GET user preference by ID 
+
+
+// TODO - DELETE user preference by ID 
+
+
+// TODO - UPDATE user preference by ID 
--- preferences/preference-router.js
+++ preferences/preference-router.js
@@ -2,3 +2,5 @@ const express = require('express')
 const router = express.Router()
 const Preferences = require('./preference-model.js')
 const Users = require('../users/user-model.js')
+
+
