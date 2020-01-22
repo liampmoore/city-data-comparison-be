127259b4d556018504b9abf270477e3d22824866
Kevin Nguyen <nguyenkevin225@gmail.com>
2020-01-22 14:00:43 -0800

    preference router completed

--- preferences/preference-router.js
+++ preferences/preference-router.js
@@ -1,21 +1,62 @@
 const express = require('express')
 const router = express.Router()
 const Preferences = require('./preference-model.js')
-const Users = require('../users/user-model.js')
+
 
 // TODO - GET all user preferences --- /api/preferences
+router.get('/', (req,res) => {
+     Preferences
+     .findAll()
+     .then(preferences => {
+          res.status(201).json(preferences)
+     })
+     .catch(error => {
+          res.status(500).json(error)
+     })
+})
 
+// TODO - GET user preference by preference ID --- /api/preferences/:id
+router.get('/:id', (req, res) => {
+     const id = req.params.id
 
-// TODO - GET user preference by ID --- /api/preferences/:id
-
+     Preferences
+     .findById(id)
+     .then(preferences => {
+          res.status(201).json(preferences)
+     })
+     .catch(error => {
+          res.status(500).json(error)
+     })
+})
 
 // TODO - DELETE user preference --- /api/preferences/:id
+router.delete('/:id', (req, res) => {
+     const id = req.params.id
 
+     Preferences
+     .remove(id)
+     .then(deleted => {
+          res.status(201).json(deleted)
+     })
+     .catch(error => {
+          res.status(500).json(error)
+     })
+})
 
 // TODO - PUT user preference --- /api/preferences/:id
+router.put('/:id', (req, res) => {
+     const id = req.params.id
+     const updatePreferences = req.body 
 
-
-
+     Preferences
+     .update(id, updatePreferences)
+     .then(updated => {
+          res.status(201).json(updated)
+     })
+     .catch(error => {
+          res.status(500).json(error)
+     })
+})
 
 
 module.exports = router;
