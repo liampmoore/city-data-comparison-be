ff6ebd832ec90c469fb8403588e4c4329a967f0f
thejohnwatt <j.watt10@gmail.com>
2020-01-26 15:19:19 -0800

    added login to reutrn users id

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -52,6 +52,7 @@ router.post('/login', (req, res) => {
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
        const token = generateToken(user) 
+       const id = user.id
         res.status(200).json({
           message: `Welcome ${user.username}!`, token, id
         });
