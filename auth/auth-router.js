7b1986bdf47caba692f0b1dae5df7329f42a5656
Michael Harms <michael.harms6010@gmail.com>
2020-01-28 11:24:11 -0600

    separates token and user obj

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -54,7 +54,7 @@ router.post('/login', (req, res) => {
        const token = generateToken(user) 
        const id = user.id
         res.status(200).json({
-          ...user, token
+          user, token
         });
       } else {
         res.status(401).json({message: "Invalid Credentials"})
