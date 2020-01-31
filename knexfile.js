c794a1411bc8eee7b5ce332bcf7bdd3583f02ecc
Michael Harms <michael.harms6010@gmail.com>
2020-01-30 20:03:40 -0600

    begins pg migration

--- knexfile.js
+++ knexfile.js
@@ -25,21 +25,18 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     }
   }
-
 };
