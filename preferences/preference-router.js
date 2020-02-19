69bf833c9e895374587f0d9ff6f8eb2afb9a3437
Kyle Porter <kyleporter@Kyles-Air.attlocal.net>
2020-02-19 14:02:10 -0500

    update for jwt in preferences-router

--- preferences/preference-router.js
+++ preferences/preference-router.js
@@ -1,61 +1,64 @@
-const express = require('express')
-const router = express.Router()
-const Preferences = require('./preference-model.js')
+const express = require('express');
+const router = express.Router();
+const Preferences = require('./preference-model.js');
+
+//Middleware
+const authMiddleware = require('../auth/requires-auth-middleware');
 
 
 // TODO - GET all user preferences --- /api/preferences
-router.get('/', (req,res) => {
+router.get('/ds', (req,res) => {
      Preferences
-     .findAll()
-     .then(preferences => {
-          res.status(201).json(preferences)
-     })
-     .catch(error => {
-          res.status(500).json(error)
-     })
+          .findAll()
+               .then(preferences => {
+                    res.status(201).json(preferences)
+               })
+               .catch(error => {
+                    res.status(500).json(error)
+               })
 })
 
 // TODO - GET user preference by preference ID --- /api/preferences/:id
-router.get('/:id', (req, res) => {
-     const id = req.params.id
+router.get('/', authMiddleware, (req, res) => {
+     const id = req.body.user_id;
 
      Preferences
-     .findById(id)
-     .then(preferences => {
-          res.status(201).json(preferences)
-     })
-     .catch(error => {
-          res.status(500).json(error)
-     })
+          .findById(id)
+               .then(preferences => {
+                    res.status(201).json(preferences)
+               })
+               .catch(error => {
+                    res.status(500).json(error)
+               })
 })
 
 // TODO - DELETE user preference --- /api/preferences/:id
-router.delete('/:id', (req, res) => {
-     const id = req.params.id
+router.delete('/', authMiddleware, (req, res) => {
+     const id = req.body.user_id;
 
      Preferences
-     .remove(id)
-     .then(deleted => {
-          res.status(201).json(deleted)
-     })
-     .catch(error => {
-          res.status(500).json(error)
-     })
+          .remove(id)
+               .then(deleted => {
+                    res.status(201).json(deleted)
+               })
+               .catch(error => {
+                    res.status(500).json(error)
+               })
 })
 
 // TODO - PUT user preference --- /api/preferences/:id
-router.put('/:id', (req, res) => {
-     const id = req.params.id
+router.put('/', authMiddleware, (req, res) => {
+     const id = req.body.user_id;
      const updatePreferences = req.body 
 
      Preferences
-     .update(id, updatePreferences)
-     .then(updated => {
-          res.status(201).json(updated)
-     })
-     .catch(error => {
-          res.status(500).json(error)
-     })
+          .update(id, updatePreferences)
+               .then(updated => {
+                    res.status(201).json(updated)
+               })
+               .catch(error => {
+                    res.status(500).json(error)
+               })
 })
 
 
